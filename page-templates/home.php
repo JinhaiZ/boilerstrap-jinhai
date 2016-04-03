cf3df4648e2c782ae0345e7c99049b6185c930d0
jinhai <zhoujinhaixd@gmail.com>
2016-04-03 23:36:38 +0200

    fix typos in home.php

--- page-templates/home.php
+++ page-templates/home.php
@@ -59,14 +59,14 @@ get_sidebar( 'mobile' ); ?>
                     <img src="http://www.jinhaizhou.com/wp-content/uploads/2016/03/c2w.png"  width ="100%">
                     <div class="carousel-caption">
                       <h4>Chat While Watching</h4>
-                      <p>Redacted an application-layer protocol specification, implemented the protocol with a simple but reliable chat room application using Python and Socket Programming</p>
+                      <p>Redacted an application-layer protocol specification, implemented the protocol with a simple but reliable chat room application using Python and Python Twisted framework</p>
                     </div>
                   </div>
                   <div class="item">
                     <img src="http://www.jinhaizhou.com/wp-content/uploads/2016/03/3d_recognition.png"  width ="100%">
                     <div class="carousel-caption">
                       <h4>3D object recognition</h4>
-                      <p>Contributed to an existing project which used deep learning for 3D object classification, modified the code in Matlab to classify customized 3D images captured by a Kinect camera</p>
+                      <p>Contributed to an existing project which used Deep Learning for 3D object classification, modified the code in Matlab to classify customized 3D images captured by a Kinect camera</p>
                     </div>
                   </div>
                 </div>
@@ -79,7 +79,7 @@ get_sidebar( 'mobile' ); ?>
   <div class="tab-pane fade in" id="profile">
   <!-- Profile================================================== -->
     <p>
-    My name is Jinhai and I am currently a second year student at <a title="" href="https://en.wikipedia.org/wiki/%C3%89cole_nationale_sup%C3%A9rieure_des_t%C3%A9l%C3%A9communications_de_Bretagne" data-original-title="Telecom Bretagne (formerly known as ENST Bretagne) is one of the top French grande école and a high standard research centre providing high level training in Information Technologies and telecommunications. This grande école of engineering is located near Brest in Brittany." data-toggle="tooltip"> Telecom Bretagne</a> in France, majoring in Computer Science and Networks. I am interested in being a software engineer in the future because I want to achieve something unusual that can improve our daily lives. To make my dream come true, I will begin with taking <a title="" href="http://stage.ooreka.fr/comprendre/annee-cesure"data-original-title="At French grande école, a year of caesura (Année césure) is a gap year between second year and third year, for the aim of doing some practical projects within the industrial world before graduation" data-toggle="tooltip">a year of caesura</a> in technology industry and making my first contribution to the development of industrial world as well as strengthen my skills of solving problems and skills of programing.
+    My name is Jinhai and I am currently a second year student at <a title="" href="https://en.wikipedia.org/wiki/%C3%89cole_nationale_sup%C3%A9rieure_des_t%C3%A9l%C3%A9communications_de_Bretagne" data-original-title="Telecom Bretagne (formerly known as ENST Bretagne) is one of the top French grande école and a high standard research centre providing high level training in Information Technologies and telecommunications. This grande école of engineering is located near Brest in Brittany." data-toggle="tooltip"> Telecom Bretagne</a> in France, majoring in Computer Science and Networks. I am interested in being a software engineer in future because I want to achieve something unusual that can improve our daily lives. To make my dream come true, I will begin with taking <a title="" href="http://stage.ooreka.fr/comprendre/annee-cesure"data-original-title="At French grande école, a year of caesura (Année césure) is a gap year between second year and third year, for the aim of doing some practical projects within the industrial world before graduation" data-toggle="tooltip">a year of caesura</a> in technology industry and making my first contribution to the development of industrial world as well as strengthen my skills of solving problems and skills of programing.
 </p>
 <!-- Profile More================================================== -->
 <p class="center">
@@ -89,13 +89,13 @@ get_sidebar( 'mobile' ); ?>
   <div class="hero-unit">
     <h2 class="center">My life path</h2>
     <p>
-    Before I came to France, I studied at <a title="" href="https://en.wikipedia.org/wiki/Xidian_University"data-original-title="" data-toggle="tooltip">Xidian University</a>, involved in <a title="Click here to see more about this project, available only in Chinese" href="https://en.wikipedia.org/wiki/Xidian_University"data-original-title="" data-toggle="tooltip">Excellent Engineer Training Plan Project</a>, so I have learned fundamental courses both in Computer Science, Electronic Engineering and Telecommunications. Among all the courses that I have taken, C Programming, Data Structure and Algorithms and Software Engineering were the three that I liked best. 
+    Before I came to France, I studied at <a title="" href="https://en.wikipedia.org/wiki/Xidian_University"data-original-title="" data-toggle="tooltip">Xidian University</a>, involved in <a title="Click here to see more about this project, available only in Chinese" href="https://en.wikipedia.org/wiki/Xidian_University"data-original-title="" data-toggle="tooltip">Excellent Engineer Training Program</a>, so I have learned fundamental courses both in Computer Science, Electronic Engineering and Telecommunications. Among all the courses that I have taken, C Programming, Data Structure and Algorithms and Software Engineering were the three that I liked most. 
     </p>
     <p>
     The reason why I like programming so much lies in my passion of creating something out of nothing. I can clearly remembered that when I was at primary school, I used to spend several hours a day to create my own campaigns in the war3 editor and that’s also when I had my first contact with programming langue such as C and Delphi. As time goes by, my passion of creating grows stronger, when I was in Xidian University, I had participated several Mathematical Modeling Contests, including the <a title="" href="https://en.wikipedia.org/wiki/Mathematical_Contest_in_Modeling"data-original-title="The Mathematical Contest in Modeling (MCM) is a multi-day mathematics competition held annually in USA, during the first or second weekend in February, since 1985 by COMAP" data-toggle="tooltip">MCM</a> with my teammates, finally, we had brilliant results from University level prize to National level prize. 
     </p>
     <p>
-    Last year, my focus turned on Machine Learning which I self-studied on Coursera. Furthermore, last semester, I had a chance to follow an experimental course of research with researchers of Telecom Bretagne. I am always keen to learning something new and thus creating something new, and that’s one reason why I open this personal site.
+    Last year, my focus turned on Machine Learning and I studied it on Coursera. Furthermore, last semester, I had a chance to follow an experimental course of research with researchers of Telecom Bretagne. I am always keen to learning something new and thus creating something new, and that’s one reason why I open this personal site.
     </p>
   </div>
 </div>
